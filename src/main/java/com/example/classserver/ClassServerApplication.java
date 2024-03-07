a7918cafbfee14beb5946c583e625ffaeef05818
Yurii Ostapchuk <yurii.ostapchuk@student.umb.sk>
2024-03-07 22:31:56 +0100

    Initial commit for backend

---
+++ src/main/java/com/example/classserver/ClassServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.classserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassServerApplication.class, args);
+    }
+
+}
