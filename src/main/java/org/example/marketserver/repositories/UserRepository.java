63d20dc0b8ae5509ba2239f8e2b4a3cf7b0b2c03
Yurii Ostapchuk <yurii.ostapchuk@student.umb.sk>
2024-03-24 22:40:59 +0100

    Modified imports and packages in UserRepository.java

--- src/main/java/org/example/marketserver/repositories/UserRepository.java
+++ src/main/java/org/example/marketserver/repositories/UserRepository.java
@@ -4,5 +4,4 @@ import org.example.marketserver.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Additional query methods can be declared here
 }
