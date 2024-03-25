db3d5a907454e15e880406d30d2fccf5b44352e2
Yurii Ostapchuk <yurii.ostapchuk@student.umb.sk>
2024-03-25 11:55:45 +0100

    Already working Postman requests fixes SecurityConfig.java to give access with work with it

--- src/main/java/org/example/marketserver/config/SecurityConfig.java
+++ src/main/java/org/example/marketserver/config/SecurityConfig.java
@@ -1,11 +1,25 @@
-package org.example.marketserver.config; // This reflects the new sub-package for configurations
+package org.example.marketserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests()
+                .anyRequest().permitAll();
+        return http.build();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
