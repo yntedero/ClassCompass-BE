e7c70dc253b7519e8a59e79707b0dd34efd62526
Yurii Ostapchuk <yurii.ostapchuk@student.umb.sk>
2024-03-30 09:43:54 +0100

    csrf security added + bcrypt pass encoder

---
+++ src/main/java/org/example/marketserver/security/core/SecurityConfig.java
@@ -0,0 +1,30 @@
+package org.example.marketserver.security.core;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) //ochrany mechanizmus, ktory sa pouziva na ochranu pred CSRF utokmi
+                .authorizeHttpRequests(auth ->
+                                auth.anyRequest().authenticated() // kazda poziadavka musi byt overena
+                        //.requestMatchers("/api/customers/**").permitAll()
+                );
+        http.addFilterBefore(new MarketAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class); // pridanie filtra
+        return http.build(); // vytvorenie filtra
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
